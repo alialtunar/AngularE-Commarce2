b874893c726b1053363e5e31fa6c3703c90906ec
acaracar12 <96421071+acaracar12@users.noreply.github.com>
2023-08-03 18:36:36 +0300

    Generic Repository yapısı kuruldu

--- API.Infrastructure/DataContext/StoreContext.cs
+++ API.Infrastructure/DataContext/StoreContext.cs
@@ -6,12 +6,16 @@ using System.Text;
 
 namespace API.Infrastructure.DataContext
 {
-   public class StoreContext: DbContext
+    public class StoreContext : DbContext
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
 
         }
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
     }
 }
---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using API.Core.DbModels;
+using API.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //private readonly IProductRepository _reopository;
+        private readonly IGenericRepository<Product> _productRepository;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepository;
+        private readonly IGenericRepository<ProductType> _productTypeRepository;
+
+        public ProductsController(IGenericRepository<Product> productRepository,
+            IGenericRepository<ProductType> productTypeRepository,
+            IGenericRepository<ProductBrand> productBrandRepository)
+        {
+            _productRepository = productRepository;
+            _productBrandRepository = productBrandRepository;
+            _productTypeRepository = productTypeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var data = await _productRepository.GetAllAsync();
+            return Ok(data);
+        }
+
+
+        [HttpGet("{id}")]
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+
+    }
+}
