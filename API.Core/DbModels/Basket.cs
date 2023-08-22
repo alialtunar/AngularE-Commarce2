73797a33115497a7eb3b935828acdc9a53beb78a
alialtunar <altunarali284@gmail.com>
2023-08-22 19:41:05 +0300

    Basket Entitiyleri Oluşturuldu

---
+++ API.Core/DbModels/Basket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Core.DbModels
+{
+    class Basket
+    {
+        public Basket()
+        {
+
+        }
+
+        public Basket(string id)
+        {
+
+        }
+
+        public string Id { get; set; }
+
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
---
+++ API.Core/DbModels/BasketItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Core.DbModels
+{
+    class BasketItem:BaseEntity
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string ProductType { get; set; }
+
+        public string ProductBrand { get; set; }
+    }
+}
